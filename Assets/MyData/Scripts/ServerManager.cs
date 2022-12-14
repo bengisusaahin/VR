bdcf59ddd19cf5ac11dc50adc03c7d60a5e945f0
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2022-12-14 14:52:00 +0300

    Added Photon Package

---
+++ Assets/MyData/Scripts/ServerManager.cs
@@ -0,0 +1,44 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServerManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        Debug.Log("Servera bağlanıldı");
+        Debug.Log("Lobiye bağlanılıyor");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        Debug.Log("Lobiye bağlanıldı");
+        Debug.Log("Odaya bağlanılıyor");
+        PhotonNetwork.JoinOrCreateRoom("Odaismi", new RoomOptions { MaxPlayers = 2, IsOpen = true, IsVisible = true }, TypedLobby.Default);
+
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        Debug.Log("Odaya bağlanıldı");
+        Debug.Log("Karakter oluşturuluyor...");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
