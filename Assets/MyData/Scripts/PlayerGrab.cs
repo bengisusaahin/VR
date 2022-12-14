f78fa01c8b8690281ebd99dcf272af1595b16a14
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2022-12-14 03:33:23 +0300

    Created the environment

---
+++ Assets/MyData/Scripts/PlayerGrab.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGrab : MonoBehaviour
+{
+    public GameObject ball;
+    public GameObject myHand;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            ball.transform.SetParent(myHand.transform);
+            ball.transform.localPosition = myHand.transform.localPosition;
+        }
+
+    }
+}
